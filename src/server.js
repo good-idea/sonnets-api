da9a8957c422108fc9719ebe1c35ebebc8fc4638
Joseph Thomas <joseph@good-idea.studio>
2019-04-16 19:31:35 -0700

    remove stray weather data
    where on earth did that come from

--- src/server.js
+++ src/server.js
@@ -11,16 +11,3 @@ app.get('/api/sonnets/:number', getSonnet)
 app.listen('3030', () => {
 	console.log('🖋 Sonnets API running on port 3000')
 })
-
-const { data: weatherData = {}, notFound } = this.state
-const {
-	name: cityName = '',
-	main: { temp, pressure } = {},
-	weather: weatherArray = [],
-	coord = {},
-	dt = Date.now(),
-} = weatherData
-
-const { icon } = weatherArray[0] || [{ icon: '01d' }]
-
-const cityCenter = [coord.lat || 51, coord.lon || -0.1]
